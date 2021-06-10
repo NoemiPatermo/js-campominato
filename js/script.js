46e2a0e7160e087bdd48b17cc9f30272d08363ff
noemi86-cpu <noemipatermo@gmail.com>
2021-06-10 14:43:17 +0200

    js-campominato

--- js/script.js
+++ js/script.js
@@ -5,3 +5,31 @@
 //Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 //La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 //Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+        //DRITTE DI OTTAVIO
+/*generiamo 16 numeri => creo funzione => while array.length < 16, 
+perché di volta in volta pusho nell'array il numero generato, solo se non è già presente
+--- 
+    0. while listNum.length <= 16
+    1.      genero un numero randomico
+    2.      if num not in array then, lo pusho nell'array listNum
+
+ora listNum contiene 16 numeri NON duplicati.
+
+while userNums.length <= 84
+    chiedi all'utente un numero 
+    if num (quello inserito dall'utente) not in array userNums:
+        if num in array listNum (numeri generati dal computer), then break. // il gioco è finito, si stoppa il ciclo.
+        else push num to userNums => next step.
+
+risultato, stampo lo score che è userNums.length.*/
+
+//genero numeri casuali tra 1 e 100
+var min= 1;
+var max = 100;
+function getRandomNumber(min, max){
+    var result= Math.floor(Math.random() * max) + min;
+    return result;
+}
+var randomNumPc = getRandomNumber(1,100);
+document.getElementById("result").innerText = "Il numero random del pc è  " + randomNumPc;
