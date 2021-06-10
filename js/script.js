caefae039c14a509d972c0dc21baf25c46c74fc9
noemi86-cpu <noemipatermo@gmail.com>
2021-06-10 22:09:35 +0200

    js-campominato

--- js/script.js
+++ js/script.js
@@ -6,23 +6,6 @@
 //La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 //Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
-        //DRITTE DI OTTAVIO
-/*generiamo 16 numeri => creo funzione => while array.length < 16, 
-perché di volta in volta pusho nell'array il numero generato, solo se non è già presente
---- 
-    0. while listNum.length <= 16
-    1.      genero un numero randomico
-    2.      if num not in array then, lo pusho nell'array listNum
-
-ora listNum contiene 16 numeri NON duplicati.
-
-while userNums.length <= 84
-    chiedi all'utente un numero 
-    if num (quello inserito dall'utente) not in array userNums:
-        if num in array listNum (numeri generati dal computer), then break. // il gioco è finito, si stoppa il ciclo.
-        else push num to userNums => next step.
-
-risultato, stampo lo score che è userNums.length.*/
 
 //GENERO  16 NUMERI CASUALI TRA 1 e 100
 
@@ -45,23 +28,26 @@ while (listaNumRandom.length < 16){
 }
 document.getElementById("result1").innerHTML = " Questo è il tuo array : "  + listaNumRandom;//contiene 16 numeri NON duplicati.
 
-
-var listaUserNum = []; //chiedi all'utente di inserire un numero e verifichi se il numero è già presente nell'array.
-                       // se il numero è presente, scatta alert
+var score = 1;
+var listaUserNum = []; 
+                       
 
 while (listaUserNum.length < 5){ //qui è 84
     var userNumb = parseInt(prompt("Inserisci un numero da 1 a 100"));
 
-    if (listaNumRandom.includes(userNumb)) {
+    if (listaNumRandom.includes(userNumb)) { // SE IL NUMERO UTENTE è PRESENTE NELL'ARRAY PC, HAI PERSO
         document.getElementById("result3").innerHTML = "Mi dispiace hai perso!";
         break;
 
-    } else if(!listaUserNum.includes(userNumb)){ 
+    } else if(!listaUserNum.includes(userNumb)){ // SE IL NUMERO UTENTE NON è PRESENTE NELL' ARRAY USER, PUSHALO
         listaUserNum.push(userNumb);
+        score += 1;
 
     } else{
         alert("inserisci un altro numero!");
+    
     }  
    
 }
-document.getElementById("result2").innerHTML = "I tuoi numeri sono: " + listaUserNum + "</br>";
+document.getElementById("result2").innerHTML = "I tuoi numeri sono: " + listaUserNum + "</br>" + "Il tuo punteggio è: " + score;
+
